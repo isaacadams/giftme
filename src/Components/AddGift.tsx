79df98376fec6c49d4af3ac92477c4ad65329fa9
Isaac Adams <iadams@emoneyadvisor.com>
2020-03-24 21:39:40 -0400

    adding gifts to list though an input

---
+++ src/Components/AddGift.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import { useState } from 'react';
+import "@isaacadams/extensions";
+
+interface IProp {
+    addGift: (gift: string) => void;
+}
+
+export function Gifts(props: IProp) {
+    
+    let [ gifts, setGifts ] = useState([]);
+    let [ newGift, setNewGift ] = useState('');
+
+    return (
+        <div>
+            <form 
+                style={{ display: "flex" }} 
+                onSubmit={onSubmit}>
+                <input 
+                    style={{ flex: 1 }} 
+                    type="text" 
+                    value={newGift}
+                    onChange={e => setNewGift(e.target.value)} />
+            </form>
+            
+            <ul>
+                {gifts.map((g, i) => 
+                    <li key={i}>{g}</li>
+                )}
+            </ul>
+        </div>
+    );
+
+    function onSubmit(e) {
+        e.preventDefault();
+        if(newGift.isNullOrWhitespace()) return;
+        setGifts([ newGift, ...gifts ]);
+    }
+}
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Gifts } from './Components/AddGift';
 
 function App(props) {
     
@@ -10,12 +11,16 @@ function App(props) {
                     navbar
                 </div>
             </div>
-            <div style={{ display: "flex" }}>
+            <div style={{ display: "flex", height: "100%" }}>
                 <div style={{ flex: 2 }}>
                     left
                 </div>
                 <div style={{ flex: 8 }}>
-                    main
+                    <div style={{ display: "flex", height: "500px" }}>
+                        <div style={{ alignSelf: "center" }} >
+                            <Gifts addGift={s => console.log(s)} />
+                        </div>
+                    </div>
                 </div>
                 <div style={{ flex: 2 }}>
                     right
