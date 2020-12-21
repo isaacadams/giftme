c41f7cedfc8d6e7d59dd1bd9994e855dee65a1cd
isaacadams <isaac.d.adams@gmail.com>
2020-12-21 17:24:31 -0500

    prettified

--- src/Components/AddGift.tsx
+++ src/Components/AddGift.tsx
@@ -1,46 +1,43 @@
 import * as React from 'react';
-import { useState } from 'react';
-import "@isaacadams/extensions";
+import {useState} from 'react';
+import '@isaacadams/extensions';
 
 interface IProp {
-    addGift: (gift: string) => void;
+  addGift: (gift: string) => void;
 }
 
 export function Gifts(props: IProp) {
-    
-    let [ gifts, setGifts ] = useState([]);
-    let [ newGift, setNewGift ] = useState('');
+  let [gifts, setGifts] = useState([]);
+  let [newGift, setNewGift] = useState('');
 
-    return (
-        <div>
-            <form 
-                style={{ display: "flex" }} 
-                onSubmit={onSubmit}>
-                <input 
-                    style={{ flex: 1 }} 
-                    type="text" 
-                    value={newGift}
-                    onChange={e => setNewGift(e.target.value)} />
-            </form>
-            
-            <ul>
-                {gifts.map((g, i) => 
-                    <li key={i}>{g}</li>
-                )}
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <form style={{display: 'flex'}} onSubmit={onSubmit}>
+        <input
+          style={{flex: 1}}
+          type="text"
+          value={newGift}
+          onChange={(e) => setNewGift(e.target.value)}
+        />
+      </form>
 
-    function onSubmit(e) {
-        e.preventDefault();
-        addGift(newGift);
-        setNewGift('');
-    }
+      <ul>
+        {gifts.map((g, i) => (
+          <li key={i}>{g}</li>
+        ))}
+      </ul>
+    </div>
+  );
 
-    function addGift(gift) {
-        if(gift.isNullOrWhitespace()) return;
+  function onSubmit(e) {
+    e.preventDefault();
+    addGift(newGift);
+    setNewGift('');
+  }
 
-        setGifts([ gift.trim(), ...gifts ]);
-    }
+  function addGift(gift) {
+    if (gift.isNullOrWhitespace()) return;
+
+    setGifts([gift.trim(), ...gifts]);
+  }
 }
-
--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,26 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Gifts } from './Components/AddGift';
+import {Gifts} from './Components/AddGift';
 
 function App(props) {
-    
-    return (
-        <div style={{ height: "100%", width: "100%" }}>
-            <div style={{ display: "flex" }}>
-                <div style={{ flex: 12 }}>
-                    navbar
-                </div>
+  return (
+    <div style={{height: '100%', width: '100%'}}>
+      <div style={{display: 'flex'}}>
+        <div style={{flex: 12}}>navbar</div>
+      </div>
+      <div style={{display: 'flex', height: '100%'}}>
+        <div style={{flex: 2}}>left</div>
+        <div style={{flex: 8}}>
+          <div style={{display: 'flex', height: '500px'}}>
+            <div style={{alignSelf: 'center'}}>
+              <Gifts addGift={(s) => console.log(s)} />
             </div>
-            <div style={{ display: "flex", height: "100%" }}>
-                <div style={{ flex: 2 }}>
-                    left
-                </div>
-                <div style={{ flex: 8 }}>
-                    <div style={{ display: "flex", height: "500px" }}>
-                        <div style={{ alignSelf: "center" }} >
-                            <Gifts addGift={s => console.log(s)} />
-                        </div>
-                    </div>
-                </div>
-                <div style={{ flex: 2 }}>
-                    right
-                </div>
-            </div>
+          </div>
         </div>
-    );
+        <div style={{flex: 2}}>right</div>
+      </div>
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
