514d8968cf53935d2bfa34e03af4430a6d9717f8
isaacadams <isaac.d.adams@gmail.com>
2020-12-21 17:39:28 -0500

    replacing custom styling with gromet

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,28 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import {Box, Grid, Grommet, Header, Main} from 'grommet';
+import {grommet} from 'grommet/themes';
 import {Gifts} from './Components/AddGift';
 
 function App(props) {
   return (
-    <div style={{height: '100%', width: '100%'}}>
-      <div style={{display: 'flex'}}>
-        <div style={{flex: 12}}>navbar</div>
-      </div>
-      <div style={{display: 'flex', height: '100%'}}>
-        <div style={{flex: 2}}>left</div>
-        <div style={{flex: 8}}>
-          <div style={{display: 'flex', height: '500px'}}>
-            <div style={{alignSelf: 'center'}}>
-              <Gifts addGift={(s) => console.log(s)} />
-            </div>
-          </div>
-        </div>
-        <div style={{flex: 2}}>right</div>
-      </div>
-    </div>
+    <Grommet full theme={grommet}>
+      <Grid></Grid>
+      <Grid
+        fill="vertical"
+        style={{gridTemplateRows: 'min-content 1fr'}}
+        gap="small"
+      >
+        <Header fill="horizontal" justify="center" pad={{vertical: 'medium'}}>
+          {/* <Navigation /> */}
+        </Header>
+        <Main align="center" pad={{vertical: 'medium', horizontal: 'medium'}}>
+          {/* <Routes /> */}
+          <Gifts addGift={(s) => console.log(s)} />
+        </Main>
+        <Box>{/* <DisplayFooter /> */}</Box>
+      </Grid>
+    </Grommet>
   );
 }
 
