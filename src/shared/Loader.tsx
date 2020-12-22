a8f2224782a979dc16850c976cf5bf6e9143558f
isaacadams <isaac.d.adams@gmail.com>
2020-12-21 21:04:12 -0500

    adding shared loader component

---
+++ src/shared/Loader.tsx
@@ -0,0 +1,24 @@
+import styled, {keyframes} from 'styled-components';
+
+const spin = keyframes`
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+`;
+
+export const Loader = styled.div`
+  border: 0.2em solid rgba(0, 0, 0, 0.1);
+  border-top: 0.2em solid #767676;
+  border-radius: 50%;
+  width: 2.28571429rem;
+  height: 2.28571429rem;
+  animation: ${spin} 0.6s linear infinite;
+`;
+
+export default Loader;
+
+// CREDIT
+// https://dev.to/stephencweiss/create-a-spinner-add-a-loader-in-react-4ic2
