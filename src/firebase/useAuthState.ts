c8f016121bc99a3c0bb5af30de42049b791ae84b
isaacadams <isaac.d.adams@gmail.com>
2020-12-22 17:40:24 -0500

    removing console logs

--- src/firebase/useAuthState.ts
+++ src/firebase/useAuthState.ts
@@ -11,13 +11,11 @@ export function useAuthState(auth: firebase.auth.Auth): FirebaseAuthState {
   let [user, setUser] = useState<firebase.User>(null);
   let [initializing, setInitializing] = useState<boolean>(true);
   let [error, setError] = useState<string>(null);
-  console.log('loading main');
   useEffect(() => {
     let unsubscribe = auth.onAuthStateChanged((u) => {
       setUser(u);
       setInitializing(false);
     });
-    console.log('loading effect');
     return () => {
       unsubscribe();
     };
