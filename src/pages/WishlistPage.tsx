74e07ccdd0cdc1bcd2816d49e0c377463bae5654
isaacadams <isaac.d.adams@gmail.com>
2020-12-23 13:35:37 -0500

    sprucing up the wishlist view

--- src/pages/WishlistPage.tsx
+++ src/pages/WishlistPage.tsx
@@ -3,6 +3,7 @@ import {useState} from 'react';
 import '@isaacadams/extensions';
 import {Box, Text} from 'grommet';
 import {FirebaseAppContext, GiftRepository, useDataApi} from '@firebase';
+import {Gift} from 'grommet-icons';
 
 export function WishlistPage(props) {
   let [repo, setRepo] = useState<GiftRepository | null>(null);
@@ -14,14 +15,19 @@ export function WishlistPage(props) {
   }, [database, authState]);
 
   return (
-    <Box
-      gap="medium"
-      margin={{top: 'small'}}
-      pad="small"
-      border={{color: 'dark-2', size: 'xsmall'}}
-    >
-      {api &&
-        api.items.map((g, i) => <GiftItemView key={i} name={g.value.name} />)}
+    <Box>
+      <Box direction="row" gap='small'>
+        <Gift /><Text>My Wishlist</Text>
+      </Box>
+      <Box
+        gap="medium"
+        margin={{top: 'small'}}
+        pad="medium"
+        border={{color: 'dark-2', size: 'xsmall'}}
+      >
+        {api &&
+          api.items.map((g, i) => <GiftItemView key={i} name={g.value.name} />)}
+      </Box>
     </Box>
   );
 }
