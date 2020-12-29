2302b8f14af5a90d3345f0540a7e752f07b90985
isaacadams <isaac.d.adams@gmail.com>
2020-12-29 14:39:41 -0500

    placeholders

--- src/pages/GroupComponents/EditGroupPage.tsx
+++ src/pages/GroupComponents/EditGroupPage.tsx
@@ -1,6 +1,24 @@
+import {GroupModel} from '@firebase';
 import {Box} from 'grommet';
 import React from 'react';
 
-export function EditGroupPage({groupname, group}) {
-  return <Box>Editing...</Box>;
+interface IProps {
+  group: GroupModel;
+  groupname: string;
+}
+
+export function EditGroupPage({groupname, group}: IProps) {
+  let mems = Object.keys(group?.members ?? {}) ?? [];
+  return (
+    <Box>
+      <Box>Header with filter and icons</Box>
+      <Box>search</Box>
+      <Box>
+        users
+        {mems.map((m, i) => (
+          <Box key={i}>{m}</Box>
+        ))}
+      </Box>
+    </Box>
+  );
 }
