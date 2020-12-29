83fbc87c9c4d7989fc8a029a2ee13ee376b53ab1
isaacadams <isaac.d.adams@gmail.com>
2020-12-28 21:03:24 -0500

    :bug: was getting false positive on the loading property

--- src/firebase/database/useQuery.tsx
+++ src/firebase/database/useQuery.tsx
@@ -50,9 +50,9 @@ export function useQuery<T>(
   return {
     error: !errors || errors.some((e) => !!e) ? errors : undefined,
     loading:
-      !loading ||
-      loading.length < config.length ||
-      !loading.every((load) => !load),
+      (loading.length < config.length ||
+        !loading.every((load) => load === false)) ??
+      true,
     data: construct(feed),
   };
 }
