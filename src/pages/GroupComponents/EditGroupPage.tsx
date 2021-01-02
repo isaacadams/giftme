2972af81afadbff123bb543ca0f351ec55b27ed6
isaacadams <isaac.d.adams@gmail.com>
2021-01-02 12:02:34 -0500

    💄clicking on invite to group button now opens a modal which allows owners to add other users

--- src/pages/GroupComponents/EditGroupPage.tsx
+++ src/pages/GroupComponents/EditGroupPage.tsx
@@ -1,8 +1,9 @@
 import {getUser, GroupModel, TableKeyWithItem, UserModel} from '@database';
-import {Box, Button, Heading} from 'grommet';
+import {Box, Button, Heading, Layer} from 'grommet';
 import {Trash} from 'grommet-icons';
 import React, {useEffect, useState} from 'react';
 import {CustomList, ShowAvatar} from '@shared';
+import {InviteToGroup} from './InviteToGroup';
 
 interface IProps {
   group: GroupModel;
@@ -10,6 +11,7 @@ interface IProps {
 }
 
 export function EditGroupPage({groupname, group}: IProps) {
+  let [showInvite, setShowInvite] = React.useState(false);
   let [members, setMembers] = useState<TableKeyWithItem<UserModel>[]>([]);
   useEffect(() => {
     Object.keys(group?.members ?? {}).forEach((k) => {
@@ -20,39 +22,55 @@ export function EditGroupPage({groupname, group}: IProps) {
   }, []);
 
   return (
-    <Box width="large">
-      <Box
-        direction="row"
-        align="center"
-        height={{max: 'medium'}}
-        justify="between"
-        margin={{bottom: 'small'}}
-      >
-        <Heading style={{margin: '0'}} level="3">
-          Manage members
-        </Heading>
-        <Box justify="end">
-          <Button primary label="Invite a member" size="small" />
+    <>
+      {showInvite && (
+        <Layer onEsc={closeInviteModal} onClickOutside={closeInviteModal}>
+          <InviteToGroup />
+        </Layer>
+      )}
+      <Box width="large">
+        <Box
+          direction="row"
+          align="center"
+          height={{max: 'medium'}}
+          justify="between"
+          margin={{bottom: 'small'}}
+        >
+          <Heading style={{margin: '0'}} level="3">
+            Manage members
+          </Heading>
+          <Box justify="end">
+            <Button
+              primary
+              label="Invite a member"
+              size="small"
+              onClick={() => setShowInvite(true)}
+            />
+          </Box>
         </Box>
+        <CustomList
+          items={[
+            //'search',
+            <>
+              {members.map((m, i) => (
+                <UserListItem
+                  {...{
+                    member: m.value.displayName ?? `@${m.value.username}`,
+                    role: group.owner === m.key ? 'owner' : 'member',
+                    key: i,
+                  }}
+                />
+              ))}
+            </>,
+          ]}
+        />
       </Box>
-      <CustomList
-        items={[
-          'search',
-          <>
-            {members.map((m, i) => (
-              <UserListItem
-                {...{
-                  member: m.value.displayName ?? `@${m.value.username}`,
-                  role: group.owner === m.key ? 'owner' : 'member',
-                  key: i,
-                }}
-              />
-            ))}
-          </>,
-        ]}
-      />
-    </Box>
+    </>
   );
+
+  function closeInviteModal() {
+    setShowInvite(false);
+  }
 }
 
 function UserListItem({member, role}) {
--- src/pages/GroupComponents/InviteToGroup.tsx
+++ src/pages/GroupComponents/InviteToGroup.tsx
@@ -1,15 +1,41 @@
-import {AddifyIcon} from '@shared';
-import {Box, Button} from 'grommet';
-import {Add, User} from 'grommet-icons';
-import React from 'react';
+import {Box, Button, Heading, TextInput} from 'grommet';
+import {Search} from 'grommet-icons';
+import React, {useState} from 'react';
+
+// search for existing user to invite
+// if none, invite to app
+// https://firebase.google.com/docs/auth/web/email-link-auth?authuser=0#completing_sign-in_in_a_web_page
+// ^ code to run when users clicks on link in email
+// auth.sendSignInLinkToEmail('some@email.com', {url: 'https://giftme-8e917.web.app/', handleCodeInApp: true});
 
 export function InviteToGroup({}) {
+  //let auth = FirebaseApp.auth();
+  let [isSelected, setIsSelected] = useState();
   return (
-    <Box>
-      <Button
-        icon={<AddifyIcon icon={<User size="large" />} />}
-        label="invite a user"
-      />
+    <Box pad="medium">
+      <Heading level="4" style={{margin: 'none'}} textAlign="center">
+        Invite member to group
+      </Heading>
+      <Box gap="small">
+        <TextInput
+          height="small"
+          placeholder="Invite by username or email"
+          icon={<Search />}
+        />
+        <Button
+          primary
+          label={
+            <Box round="xsmall" pad="xsmall" align="center">
+              {isSelected ? 'Add user to this group' : 'Select a user above'}
+            </Box>
+          }
+          focusIndicator={false}
+          plain
+          hoverIndicator
+          disabled={!isSelected}
+          onClick={() => {}}
+        />
+      </Box>
     </Box>
   );
 }
