4e1f74b25164d928ea6ffd9ccd08ffca333a540a
isaacadams <isaac.d.adams@gmail.com>
2021-01-04 18:54:53 -0500

    rendering invites and allowing users to accept them

--- src/pages/GroupComponents/GroupInvites.tsx
+++ src/pages/GroupComponents/GroupInvites.tsx
@@ -1,14 +1,22 @@
-import {useQuery} from '@database';
-import {Accordion, AccordionPanel, List} from 'grommet';
-import React, {useContext} from 'react';
+import {
+  getGroup,
+  GroupInviteHelper,
+  GroupModel,
+  TableKeyWithItem,
+  useQuery,
+} from '@database';
+import {Accordion, AccordionPanel, Box} from 'grommet';
+import {FormCheckmark, FormClose} from 'grommet-icons';
+import React, {useEffect, useState} from 'react';
 
-export function GroupInvites({userid}) {
+export function GroupInvites({userid}: {userid: string}) {
+  console.log('GroupInvites: rendering');
   let {data} = useQuery<string[]>(
     [
       {
         key: `invites/${userid}`,
         event: 'value',
-        cb: (s) => {
+        cb: (s, b) => {
           return Object.keys(s.val() ?? {});
         },
       },
@@ -17,12 +25,48 @@ export function GroupInvites({userid}) {
       return invites;
     }
   );
+
+  let [groupsInvited, setGroupsInvited] = useState<
+    TableKeyWithItem<GroupModel>[]
+  >([]);
+  useEffect(() => {
+    let isSubscribed = !!data;
+    if (isSubscribed) {
+      Promise.all(data.map((g) => getGroup(g))).then((g) => {
+        setGroupsInvited([...g, ...groupsInvited]);
+      });
+    }
+    return () => (isSubscribed = false);
+  }, [data]);
   console.log(data);
   return (
-    <Accordion margin="small">
-      <AccordionPanel label="You have group invites (#)">
-        <List data={['hello']} />
+    <Accordion margin="small" style={{border: 'none'}}>
+      <AccordionPanel label={`You have group invites (${data?.length})`}>
+        {groupsInvited.map((g, i) => (
+          <InviteButton
+            key={i}
+            name={g.displayName ?? '@' + g.name}
+            accept={() =>
+              new GroupInviteHelper(userid, g.key).confirmInviteToGroup()
+            }
+            reject={() =>
+              new GroupInviteHelper(userid, g.key).denyInviteToGroup()
+            }
+          />
+        ))}
       </AccordionPanel>
     </Accordion>
+  );
+}
+
+function InviteButton({name, accept, reject}) {
+  return (
+    <Box direction="row" pad="small" justify="between">
+      {name}
+      <Box direction="row" gap="small">
+        <FormCheckmark color="status-ok" onClick={accept} />
+        <FormClose color="status-error" onClick={reject} />
+      </Box>
+    </Box>
   );
 }
